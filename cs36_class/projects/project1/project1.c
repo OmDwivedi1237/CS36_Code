c77b5fed1f7f888b3a29532ed8a38922f4e608f1
Om Dwivedi <26dwivediom@gmail.com>
2024-06-16 15:12:18 -0700

    Completed Project 1!

---
+++ cs36_class/projects/project1/project1.c
@@ -0,0 +1,60 @@
+// Om Dwivedi - Project 1
+
+#include <stdio.h>
+
+// prototypes
+void printSquaresAndCubes();
+void printNameASCII();
+void calculateAndPrintFloatStats();
+
+int main() {
+    printSquaresAndCubes();
+    printNameASCII();
+    calculateAndPrintFloatStats();
+}
+
+//----Part 1----
+void printSquaresAndCubes() {
+    int num;
+    printf("Number\tSquare\tCube\n");
+    for (num = 1; num <= 10; num++) {
+        printf("%d\t%d\t%d\n", num, num * num, num * num * num);
+    }
+
+    printf("\n\n");
+}
+
+//----Part 2----
+void printNameASCII() {
+    char name[] = "Om Dwivedi";
+    for (int i = 0; name[i] != '\0'; i++) {
+        printf("\n%c - %d\n", name[i], name[i]);
+    }
+
+    printf("\n\n");
+}
+
+//----Part 3----
+void calculateAndPrintFloatStats() {
+    float num1, num2, num3;
+    printf("\n\nEnter three different floating-point numbers: ");
+    scanf_s("%f %f %f", &num1, &num2, &num3);
+
+    float sum = num1 + num2 + num3;
+    float average = sum / 3;
+    float product = num1 * num2 * num3;
+
+    float smallest = num1;
+    if (num2 < smallest) smallest = num2;
+    if (num3 < smallest) smallest = num3;
+
+    float largest = num1;
+    if (num2 > largest) largest = num2;
+    if (num3 > largest) largest = num3;
+
+    printf("Sum: %.2f\n", sum);
+    printf("Average: %.2f\n", average);
+    printf("Product: %.2f\n", product);
+    printf("Smallest: %.2f\n", smallest);
+    printf("Largest: %.2f\n", largest);
+}
